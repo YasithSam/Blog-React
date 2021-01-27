043f2186064f32afd098203ce6b2cd46c12355a7
YasithSam <yasith.sam7@gmail.com>
2021-01-27 15:17:25 +0530

    Initialize

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
 import logo from './logo.svg';
 import './App.css';
+import Navbar from './Navbar';
+import Home from './Home';
+
 
 function App() {
+  const title="Hello welcome to my blog!";
+  const likes=50;
+  const link="www.google.lk";
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navbar />
+      <div className="content">
+        <Home />
+        {/* <h1>{title}</h1>
+        <p>Likes {likes}</p>
+        <p>{10}</p>
+        <p>{[1,2,3,4,5]}</p> */}
+        {/*<p>{Math.random()}</p>*/}
+        {/*<a href={link}>Google</a>*/}
+      </div>
     </div>
   );
 }
