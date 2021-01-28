3f0f26ebb5a18badc1d67f5eae93b107e5c493e3
YasithSam <yasith.sam7@gmail.com>
2021-01-28 20:12:25 +0530

    Fetch data

--- src/Home.js
+++ src/Home.js
@@ -1,21 +1,27 @@
-import { useState } from "react";
+import { useState , useEffect} from "react";
 import BlogList from "./BlogList";
 
 const Home = () => {
-  const [blogs, setBlogs] = useState([
-    { title: 'My new website', body: 'lorem ipsum...', author: 'mario', id: 1 },
-    { title: 'Welcome party!', body: 'lorem ipsum...', author: 'yoshi', id: 2 },
-    { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'mario', id: 3 }
-  ])
+  const [blogs, setBlogs] = useState(null);
+  //const [name,setName]= useState('Mario');
+  const [isPending,setIsPending]=useState(true);
 
-  const handleDelete = (id) => {
-    const newBlogs = blogs.filter(blog => blog.id !== id);
-    setBlogs(newBlogs);
-  }
+  useEffect(()=> {
+     fetch(' http://localhost:8000/blogs')
+     .then(res=>{
+         return res.json();
+     })
+     .then(data =>{
+        setBlogs(data);
+        setIsPending(false);
+     });
+  },[]);
 
   return (
     <div className="home">
-      <BlogList blogs={blogs} title="All Blogs" handleDelete={handleDelete} />
+      {isPending && <p>Loading...</p>}
+      {blogs && <BlogList blogs={blogs} title="All Blogs" />}
+      {/*right happens only when left is available*/}
     </div>
   );
 }
