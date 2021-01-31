23827ca05ccc11dd2a00af7b859eb16d09ddc68f
YasithSam <yasith.sam7@gmail.com>
2021-01-31 14:41:23 +0530

    Routes

--- src/App.js
+++ src/App.js
@@ -1,26 +1,31 @@
-import logo from './logo.svg';
 import Navbar from './Navbar';
 import Home from './Home';
-
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Create from './Create';
 
 function App() {
-  const title="Hello welcome to my blog!";
-  const likes=50;
-  const link="www.google.lk";
   return (
-    <div className="App">
-      <Navbar />
-      <div className="content">
-        <Home />
-        {/* <h1>{title}</h1>
+    <Router>
+      <div className="App">
+        <Navbar />
+        <div className="content">
+          <Switch>
+            <Route exact path="/">
+              <Home />
+            </Route>
+            <Route path="/create">
+              <Create />
+            </Route>
+          </Switch>
+        </div>
+      </div>
+    </Router>
+  );
+}
+{/* <h1>{title}</h1>
         <p>Likes {likes}</p>
         <p>{10}</p>
         <p>{[1,2,3,4,5]}</p> */}
         {/*<p>{Math.random()}</p>*/}
         {/*<a href={link}>Google</a>*/}
-      </div>
-    </div>
-  );
-}
-
 export default App;
